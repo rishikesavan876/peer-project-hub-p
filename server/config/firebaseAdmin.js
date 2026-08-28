aed3792eacebf2277adaaa61840ef7b2b51e11b5
rishikesavan876 <rishikesavan876@gmail.com>
2026-08-28 12:44:03 +0530

    fix firebase admin render

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,5 +1,12 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-service-account.json');
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('../firebase-service-account.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
